5a8a30c8da8eae43a8728b5ca3b1504393debc47
jendiamond <thejendiamond@gmail.com>
2022-06-25 13:56:12 -0700

    feat: add flexible-impact-numbers-carousel

---
+++ components/Flexible/ImpactNumbersCarousel.vue
@@ -0,0 +1,23 @@
+<template>
+    <section class="impact-numbers-carousel">
+        HELLO
+        <impact-numbers-carousel v-bind="$props" />
+    </section>
+</template>
+
+<script>
+export default {
+    props: {
+        block: {
+            type: Object,
+            default: () => {},
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.impact-numbers-carousel {
+    background-color: aquamarine;
+}
+</style>
