40a5f118f45235755918e34e620acf70e4a48bbc
pghorpade <pghorpade@library.ucla.edu>
2022-07-18 15:08:31 -0700

    test: Add resource detail page e2e stub

--- cypress/integration/resourcepage.spec.js
+++ cypress/integration/resourcepage.spec.js
@@ -6,7 +6,7 @@ describe("Resource Detail page", () => {
         cy.get(".logo-ucla").should("be.visible")
         cy.get("h1.title").should(
             "contain",
-            "Printing Practices - Simple Card 3 Up - Rich Text - Pull Quote"
+            "Meap Bunnies"
         )
         cy.percySnapshot({ widths: [768, 992, 1200] })
     })
