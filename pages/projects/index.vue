32d49cb36a13d50a01a7dd5c9b7b7713f794180e
Ashton Prigge <aprigge@library.ucla.edu>
2022-08-18 13:01:35 -0700

    update queries

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,17 +1,29 @@
 <template lang="html">
     <main class="page page-project-list">
-        <masthead-secondary />
-        <divider-way-finder />
+        <masthead-secondary
+            :title="summaryData.projectListTitle"
+            :text="summaryData.projectListSummary"
+        />
+        <divider-way-finder color="help" />
         <section-teaser-card
             class="content"
             :items="projectList"
         />
-        <divider-general />
+        <divider-way-finder color="help" />
+        <section-cards-with-illustrations
+            v-if="parsedAssociatedTopics.length"
+            class="section-cards"
+            :items="parsedAssociatedTopics"
+            title="Associated Topics"
+            button-text="All Services and Resources"
+            to="/resources"
+        />
     </main>
 </template>
 
 <script>
 // GQL
+import PROJECT_LISTING_PAGE from "~/gql/queries/ProjectListingPage"
 import PROJECT_LIST from "~/gql/queries/ProjectList"
 
 // Helpers
@@ -20,10 +32,13 @@ import _get from "lodash/get"
 export default {
     async asyncData({ $graphql, params, store }) {
         // Do not remove testing live preview
-        const data = await $graphql.default.request(PROJECT_LIST, {
-            slug: params.slug,
-        })
+        const summaryData = await $graphql.default.request(
+            PROJECT_LISTING_PAGE,
+            {}
+        )
+        const data = await $graphql.default.request(PROJECT_LIST, {})
         return {
+            summaryData: _get(summaryData, "entries[0]", {}),
             page: _get(data, "entries", {}),
         }
     },
@@ -43,6 +58,16 @@ export default {
                 }
             })
         },
+        parsedAssociatedTopics() {
+            return this.summaryData.featuredMeapResources.map((obj) => {
+                return {
+                    ...obj,
+                    to: obj.externalResourceUrl
+                        ? obj.externalResourceUrl
+                        : obj.uri,
+                }
+            })
+        },
     },
 }
 </script>
@@ -51,6 +76,10 @@ export default {
 .page-project-list {
     .content {
         margin: 0 auto;
+    }
+
+    .section-cards {
+        margin: var(--space-3xl) auto;
     }
 }
 </style>
