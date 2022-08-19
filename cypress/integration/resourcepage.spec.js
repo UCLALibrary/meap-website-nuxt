2f55f51317f5b7b93eb2e0f92ad58384f8838b14
Ashton Prigge <aprigge@library.ucla.edu>
2022-08-19 12:27:23 -0700

    update spec

--- cypress/integration/resourcepage.spec.js
+++ cypress/integration/resourcepage.spec.js
@@ -4,7 +4,7 @@ describe("Resource Detail page", () => {
 
         // UCLA Library brand
         cy.get(".logo-ucla").should("be.visible")
-        cy.get("h1.title").should("contain", "Picnic")
+        cy.get("h1.title").should("contain", "Picnics")
         cy.percySnapshot({ widths: [768, 992, 1200] })
     })
 })
