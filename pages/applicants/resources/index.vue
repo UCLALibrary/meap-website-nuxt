ebc49a603468194842ff724b21798015fd6145d9
jendiamond <thejendiamond@gmail.com>
2022-08-19 17:30:43 -0700

    feat: rebase

--- pages/applicants/resources/index.vue
+++ pages/applicants/resources/index.vue
@@ -1,71 +1,101 @@
 <template lang="html">
-    <div class="page page-help">
-        <h3>Services and Resources</h3>
-        <nuxt-link
-            v-for="item in parsedServiceAndResourceList"
-            :key="item.to"
-            :to="item.to"
-        >
-            <div
-                class="text"
-                v-html="item.title"
-            />
-        </nuxt-link>
-        <br>
-        <h3>Help Topics</h3>
-        <nuxt-link
-            v-for="item in parsedHelpTopicList"
-            :key="item.to"
-            :to="item.to"
-        >
-            <div
-                class="text"
-                v-html="item.title"
-            />
-        </nuxt-link>
+    <div class="page page-resources-list">
+        <masthead-secondary
+            :title="summaryData.title"
+            :text="summaryData.text"
+        />
+
+        <divider-way-finder
+            class="divider divider-first"
+            color="about"
+        />
+
+        <section-cards-with-illustrations
+            :items="resourceList"
+            :isHorizontal= "true"
+        />
+
+        <divider-way-finder
+            class="divider divider-first"
+            color="about"
+        />
+
+        <block-call-to-action
+            class="section"
+            svgName="search"
+            title="Have further questions?"
+            text="Get in touch"
+            name="Contact us"
+            to="https://google.com/map"
+            :isDark=false
+            :isSmallSize="isSmallSize"
+        />
     </div>
 </template>
-
 <script>
-// gql
+// GQL
 import RESOURCE_LIST from "~/gql/queries/ResourceList"
-import HELP_TOPIC_LIST from "~/gql/queries/HelpTopicList"
+
+// Helpers
+import _get from "lodash/get"
 
 export default {
-    async asyncData({ $graphql, params }) {
-        const data = await $graphql.default.request(RESOURCE_LIST, {
-            uri: params.path,
-        })
-        const helpTopicData = await $graphql.default.request(HELP_TOPIC_LIST, {
-            uri: params.path,
-        })
+    async asyncData({ $graphql, params, store }) {
+        const data = await $graphql.default.request(RESOURCE_LIST, {})
         return {
-            page: data,
-            helpTopic: helpTopicData,
+            summaryData: _get(data, "entry", {}),
+            page: _get(data, "entries", {}),
+        }
+    },
+    head() {
+        let title = this.summaryData
+            ? this.summaryData.resourceListTitle
+            : "... loading"
+        return {
+            title: title,
         }
     },
     computed: {
-        parsedServiceAndResourceList() {
-            return this.page.entries.map((obj) => {
+        resourceList() {
+            return this.page.map((obj) => {
                 return {
                     ...obj,
-                    to: `/applicants/resources/${obj.to}`,
+                    to: `/${obj.to}`,
+                    image: _get(obj, "image[0].image[0]", {}),
                 }
             })
         },
-        parsedHelpTopicList() {
-            return this.helpTopic.entries.map((obj) => {
-                return {
-                    ...obj,
-                    to: `/applicants/${obj.to}`,
-                }
-            })
-        },
-    },
+    }
 }
+
 </script>
 
 <style lang="scss" scoped>
-.page-help {
-}
+.page-resources-list {
+    .section-header {
+        margin-top: var(--space-3xl);
+        margin-bottom: var(--space-xl);
+    }
+    .section-title {
+        @include step-4;
+        color: var(--color-primary-blue-03);
+        margin: 0 auto;
+        max-width: $container-l-main + px;
+    }
+    .content {
+        margin: 0 auto;
+    }
+
+    .section-cards {
+        margin: var(--space-3xl) auto;
+    }
+
+    @media #{$medium} {
+        .content,
+        .section-title {
+            padding: 0 var(--unit-gutter);
+        }
+    }
+    }
 </style>
+</template>
