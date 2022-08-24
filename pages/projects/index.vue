f097464072336c706a4f0cf6e1c4c763ceec2387
Ashton Prigge <aprigge@library.ucla.edu>
2022-08-24 12:13:06 -0700

    rebase

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -7,17 +7,20 @@
 
         <section-wrapper>
             <div class="section-header">
-                <h2 class="section-title">
-                    All Projects
-                </h2>
+                <h2 class="section-title">All Projects</h2>
             </div>
             <section-teaser-card :items="projectList" />
         </section-wrapper>
 
-        <section-wrapper v-if="(parsedAssociatedTopics && parsedAssociatedTopics.length)" theme="divider" >
+        <section-wrapper
+            v-if="parsedAssociatedTopics && parsedAssociatedTopics.length"
+            theme="divider"
+        >
             <divider-way-finder color="help" />
         </section-wrapper>
-        <section-wrapper v-if="(parsedAssociatedTopics && parsedAssociatedTopics.length)" >
+        <section-wrapper
+            v-if="parsedAssociatedTopics && parsedAssociatedTopics.length"
+        >
             <section-cards-with-illustrations
                 :items="parsedAssociatedTopics"
                 title="Associated Topics"
@@ -25,11 +28,22 @@
                 to="/applicants/resources"
             />
         </section-wrapper>
+        <section-teaser-card class="content" :items="projectList" />
+        <divider-way-finder color="help" />
+        <section-cards-with-illustrations
+            v-if="parsedAssociatedTopics.length"
+            class="section-cards"
+            :items="parsedAssociatedTopics"
+            title="Associated Topics"
+            button-text="All Resources"
+            to="/applicants/resources"
+        />
     </main>
 </template>
 
 <script>
 // GQL
+import PROJECT_LISTING_PAGE from "~/gql/queries/ProjectListingPage"
 import PROJECT_LIST from "~/gql/queries/ProjectList"
 
 // Helpers
@@ -37,9 +51,20 @@ import _get from "lodash/get"
 
 export default {
     async asyncData({ $graphql, params, store }) {
+<<<<<<< HEAD
         const data = await $graphql.default.request(PROJECT_LIST, {})
         return {
             summaryData: _get(data, "entry", {}),
+=======
+        // Do not remove testing live preview
+        const summaryData = await $graphql.default.request(
+            PROJECT_LISTING_PAGE,
+            {}
+        )
+        const data = await $graphql.default.request(PROJECT_LIST, {})
+        return {
+            summaryData: _get(summaryData, "entries[0]", {}),
+>>>>>>> cfbf352 (update queries)
             page: _get(data, "entries", {}),
         }
     },
@@ -83,6 +108,28 @@ export default {
     .section-title {
         @include step-4;
         color: var(--color-primary-blue-03);
+<<<<<<< HEAD
+=======
+        margin: 0 auto;
+        max-width: $container-l-main + px;
     }
+    .content {
+        margin: 0 auto;
+    }
+
+    .section-cards {
+        margin: var(--space-3xl) auto;
+    }
+<<<<<<< HEAD
+
+    @media #{$medium} {
+        .content,
+        .section-title {
+            padding: 0 var(--unit-gutter);
+        }
+>>>>>>> 0507e24 (rebase)
+    }
+=======
+>>>>>>> cfbf352 (update queries)
 }
 </style>
