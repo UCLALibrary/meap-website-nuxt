8d52aa5fe45cd83d7d33da7a5841f6be1a5cb221
Ashton Prigge <aprigge@library.ucla.edu>
2022-08-24 15:37:19 -0700

    fix old rebase

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -30,18 +30,6 @@
                 to="/applicants/resources"
             />
         </section-wrapper>
-        <section-teaser-card
-            class="content"
-            :items="projectList"
-        />
-        <divider-way-finder color="help" />
-        <section-cards-with-illustrations
-            v-if="parsedAssociatedTopics.length"
-            class="section-cards"
-            :items="parsedAssociatedTopics"
-            title="Associated Topics"
-            button-text="All Resources"
-            to="/applicants/resources"
         />
     </main>
 </template>
