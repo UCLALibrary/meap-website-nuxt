86baaa46521d09f2ec491e8388aa676c65204c09
Ashton Prigge <aprigge@library.ucla.edu>
2022-08-24 11:19:45 -0700

    update spec

--- cypress/integration/newslistingpage.spec.js
+++ cypress/integration/newslistingpage.spec.js
@@ -1,10 +1,10 @@
-describe("Project Detail page", () => {
+describe("News Listing Page", () => {
     it("Visit News Listing Page", () => {
         cy.visit("/about/news")
 
         // UCLA Library brand
         cy.get(".logo-ucla").should("be.visible")
-        // cy.get("h1.title").should("contain", "")
+        cy.get("h1.title").should("contain", "MEAP News")
         cy.percySnapshot({ widths: [768, 992, 1200] })
     })
 })
