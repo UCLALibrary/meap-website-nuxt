1bd024e7277a87c45eb2caf6ef15d336aa4f4772
pghorpade <pghorpade@library.ucla.edu>
2022-08-24 08:47:18 -0700

    feat: Add MEAP headers with meap craft data

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-    <main class="page">
+    <section class="page">
         <masthead-secondary
             title="Modern Endangered Archives Program"
             text="Our grants enable digitization and access to at-risk cultural heritage collections from around the world. Explore our projects and learn more about available grant opportunities."
@@ -106,7 +106,7 @@
                 <button-more text="See all news" />
             </nuxt-link>
         </div>
-    </main>
+    </section>
 </template>
 
 <script>
