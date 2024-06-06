8c9a166df72e55e6a42f1c8a45c6690c4dfcbe2c
Jess Divers <jessica.divers@library.ucla.edu>
2024-06-06 09:49:37 -0700

    fix: refactor different page

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,8 +1,10 @@
 <script setup>
 // GQL
 import _get from 'lodash/get'
-import PROJECT_DETAIL from '../gql/queries/ProjectDetail.gql'
-// import GENERAL_CONTENT_DETAIL from '~/gql/queries/GeneralContentDetail'
+import GENERAL_CONTENT_DETAIL from '../gql/queries/GeneralContentDetail.gql'
+
+// Helpers
+// import stripMeapFromURI from "../utils/stripMeapFromURI"
 
 // Helpers
 const { $graphql, $getHeaders } = useNuxtApp()
@@ -14,16 +16,13 @@ const path = route.path.replace(/^\/|\/$/g, '') // trim initial and/or final sla
 // const variables = { path }
 
 // ASYNCDATA
-const { data, error } = await useAsyncData(`general-content-${path}`, async () => {
-  // TODO PROJECT_DETAIL or GENERAL_CONTENT_DETAIL like in ticket?
-  const data = await $graphql.default.request(PROJECT_DETAIL, {
-    slug: params.slug, // TO DO find example with params, is this correct?
+// TODO if errors check params documentation
+const { data, error } = await useAsyncData(`general-content-${path}`, async ({ $graphql, params }) => {
+  const data = await $graphql.default.request(GENERAL_CONTENT_DETAIL, {
+    slug: params.pathMatch.substring(
+      params.pathMatch.lastIndexOf('/') + 1
+    ),
   })
-  // const data = await $graphql.default.request(GENERAL_CONTENT_DETAIL, {
-  //   slug: params.pathMatch.substring(
-  //     params.pathMatch.lastIndexOf('/') + 1
-  //   ),
-  // })
   return { data }
 })
 
@@ -42,11 +41,11 @@ if (!data.value.entry) {
     fatal: true
   })
 }
-// INDEX
-if (data.value.entry.slug && process.server) {
-  const { $elasticsearchplugin } = useNuxtApp()
-  await $elasticsearchplugin.index(data.value.entry, path.replaceAll('/', '--'))
-}
+// INDEX - remove
+// if (data.value.entry.slug && process.server) {
+//   const { $elasticsearchplugin } = useNuxtApp()
+//   await $elasticsearchplugin.index(data.value.entry, path.replaceAll('/', '--'))
+// }
 
 // DATA
 const page = ref(_get(data.value, 'entry', {}))
@@ -65,197 +64,79 @@ useHead({
 })
 
 // COMPUTED
-const parsedButtonText = computed(() => {
-  return _get(page.value, 'meapProjectCallToAction[0].buttonText', '')
+const parseParentPageURL = computed(() => {
+  if (page.value.parent && page.value.parent.uri) {
+    console.log('page.value.parent.uri', page.value.parent.uri)
+    return `/${stripMeapFromURI(page.value.parent.uri)}`
+  }
+
+  return '/'
 })
-const parsedButtonTo = computed(() => {
-  // try to get externalUrl
-  const buttonTo = _get(
-    page.value,
-    'meapProjectCallToAction[0].externalUrl',
-    ''
-  )
-  // return externalURl, if not available, get uploadAsset url
-  return buttonTo || _get(
-    page.value,
-    'meapProjectCallToAction[0].uploadAsset[0].url',
-    ''
-  )
+const parseParentTitle = computed(() => {
+  if (page.value.parent && page.value.parent.title)
+    return page.value.parent.title
+
+  return 'Modern Endangered Archives Program'
 })
 </script>
 <template lang="html">
   <main
     id="main"
-    class="page page-project-detail"
+    class="page page-general-content"
   >
     <nav-breadcrumb
+      v-if="page"
       :title="page.title"
-      to="/projects"
-      parent-title="Funded Projects"
-      class="nav-breadcrumb"
+      class="breadcrumb"
+      :to="parseParentPageURL"
+      :parent-title="parseParentTitle"
     />
-
     <banner-text
-      v-if="!page.heroImage || page.heroImage.length == 0"
+      v-if="page && (!page.heroImage || page.heroImage.length == 0)"
       class="banner-text"
       :category="page.format"
       :title="page.title"
       :text="page.summary"
-      :button-text="parsedButtonText"
-      :to="parsedButtonTo"
     />
 
     <section-wrapper class="section-banner">
       <banner-header
-        v-if="page.heroImage && page.heroImage.length == 1"
+        v-if="page && page.heroImage && page.heroImage.length == 1"
         :image="page.heroImage[0].image[0]"
         :category="page.format"
         :title="page.title"
         :text="page.summary"
-        :to="parsedButtonTo"
-        :prompt="parsedButtonText"
       />
     </section-wrapper>
 
     <section-wrapper theme="divider">
       <divider-way-finder
         class="divider-way-finder"
-        color="help"
-      />
-    </section-wrapper>
-
-    <section-wrapper section-title="About the Project">
-      <div
-        class="title-general"
-        v-html="page.titleGeneral"
-      />
-
-      <div
-        v-if="page.projectType"
-        class="project-type"
-        v-html="page.projectType"
-      />
-
-      <rich-text
-        v-if="page.projectDescription"
-        :rich-text-content="page.projectDescription"
-        class="project-description"
-      />
-
-      <rich-text
-        v-if="page.citation"
-        :rich-text-content="page.citation"
-        class="citation"
-      />
-
-      <divider-general
-        v-if="page.projectContributors && page.projectDescription"
-        class="divider-general"
-      />
-
-      <h3
-        v-if="page.projectContributorsSubheading"
-        class="contributors-subheading"
-        v-html="page.projectContributorsSubheading"
-      />
-
-      <rich-text
-        v-if="page.projectContributors"
-        class="contributors-content"
-        :rich-text-content="page.projectContributors"
-      />
-
-      <h3
-        v-if="page.institution"
-        class="institution-subheading"
-      >
-        Host Institution
-      </h3>
-
-      <rich-text
-        v-if="page.institution"
-        class="institution-content"
-        :rich-text-content="page.institution"
-      />
-    </section-wrapper>
-
-    <section-wrapper theme="divider">
-      <divider-way-finder
-        class="divider-way-finder"
-        color="help"
+        color="visit"
       />
     </section-wrapper>
 
     <flexible-blocks
-      v-if="page.blocks && page.blocks.length"
-      class="content"
+      v-if="page"
+      class="flexible-content"
       :blocks="page.blocks"
     />
-
-    <section-wrapper theme="divider">
-      <divider-way-finder
-        v-if="page.blocks && page.blocks.length"
-        class="divider-way-finder"
-        color="help"
-      />
-    </section-wrapper>
-
-    <section-wrapper>
-      <block-call-to-action
-        class="block-call-to-action"
-        :is-meap-global="true"
-      />
-    </section-wrapper>
   </main>
 </template>
 <style lang="scss" scoped>
-.page-project-detail {
+.page-general-content {
 
   .banner-text,
   .banner-header {
-    --color-theme: var(--color-help-green-03);
+    --color-theme: var(--color-visit-fushia-03);
   }
 
-  .section-banner.section-wrapper.theme-white {
+  :deep(.divider-way-finder) {
+    --color-border: var(--color-visit-fushia-03);
+  }
+
+  .section-banner {
     margin-top: 0;
-  }
-
-  .title-general {
-    @include step-1;
-    color: var(--color-secondary-grey-05);
-    max-width: $container-l-main + px;
-    margin: 0 auto 12px;
-  }
-
-  .project-type {
-    @include step-0;
-    font-weight: $font-weight-medium;
-    color: var(--color-primary-blue-03);
-    text-transform: capitalize;
-    max-width: $container-l-main + px;
-    margin: 0 auto var(--space-m);
-  }
-
-  .citation {
-    margin-top: var(--space-m);
-
-    :deep p {
-      color: var(--color-secondary-grey-04);
-    }
-  }
-
-  .contributors-subheading,
-  .institution-subheading {
-    @include step-1;
-    max-width: $container-l-main + px;
-    margin: 0 auto var(--space-m);
-    color: var(--color-secondary-grey-05);
-  }
-
-  .contributors-content,
-  .institution-content {
-    @include step-0;
-    margin-bottom: var(--space-xl);
   }
 }
 </style>
