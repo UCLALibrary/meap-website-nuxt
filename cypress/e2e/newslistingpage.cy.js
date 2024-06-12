1077e0cf2afaf771ae51a3b8bcc58f4b6c7e37f1
tinuola <tinuola@users.noreply.github.com>
2024-06-12 11:34:44 -0700

    update e2e test

--- cypress/e2e/newslistingpage.cy.js
+++ cypress/e2e/newslistingpage.cy.js
@@ -7,4 +7,13 @@ describe('News Listing Page', () => {
     cy.get('h1.title').should('contain', 'MEAP News')
     cy.percySnapshot({ widths: [768, 992, 1200] })
   })
+  it('Search Found', () => {
+    cy.visit('/about/news?q=kids&filters=')
+    cy.get('.logo-ucla').should('be.visible')
+    cy.get('input[type=search]').should(
+      'have.value',
+      'kids'
+    )
+    cy.get('h2.about-results').invoke('text').should('not.be.empty')
+  })
 })
