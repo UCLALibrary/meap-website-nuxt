9eba10ecc386142b4da0d5f86d989dc1422ca995
pghorpade <pghorpade@library.ucla.edu>
2026-02-05 11:29:20 -0800

    fix: update unsused plugin

--- plugins/init.server.js
+++ plugins/init.server.js
@@ -1,10 +1,7 @@
 export default defineNuxtPlugin(async (nuxtApp) => { //
   // console.log('init.server.js start')
   /* const craftGlobals = useGlobalStore() //
-  await craftGlobals.fetchGlobals()
-  await craftGlobals.fetchHeader()
-  await craftGlobals.fetchFooterPrimary()
-  await craftGlobals.fetchFooterSock()
-  await craftGlobals.fetchFooterSponsor() */
+  await craftGlobals.fetchGlobals()*/
   // console.log('init.server.js end')
+  // This plugin is no more used as we are now fteching using useasyncdata in composable for the layout
 })
