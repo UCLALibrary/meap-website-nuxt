8e5397d24922236f300a442de70beba531e5ea8c
jendiamond <thejendiamond@gmail.com>
2026-05-19 14:23:38 -0700

    feat: add title tag to all pages

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,29 @@
 <script setup>
 // No Theme like Main Library Website
 const { enabled, state } = usePreviewMode()
+
+// set title
+useHead({
+  titleTemplate: (titleChunk) => {
+    // If undefined or blank then we don't need the pipe and space
+    return titleChunk === 'Homepage'
+      ? 'Modern Endangered Archives Program '
+      : `${titleChunk || 'Error'}  | Modern Endangered Archives Program `
+  },
+  htmlAttrs: {
+    lang: 'en',
+  },
+  meta: [
+    { charset: 'utf-8' },
+    {
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1.0, minimum-scale=1.0',
+    },
+  ],
+  link: [
+    { rel: 'icon', type: 'image/svg', href: '/icon-ucla-favicon.svg' },
+  ],
+})
 </script>
 <template>
   <div>
